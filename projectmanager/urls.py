10e1ebc9e8cf66054f401b4d8f3158742c43d7d5
cbelotto <belottocesar@fpuna.edu.py>
2021-08-14 17:36:07 -0400

    vista agregada

--- projectmanager/urls.py
+++ projectmanager/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-
+from .views import homepage
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard/home.html'),name='home'),
+    path('', homepage,name="home"),
 ]
