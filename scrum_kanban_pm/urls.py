49405ff80aff931ef8fe1c5c214f77b2ee0936f8
Nelson <ruizn291@gmail.com>
2021-08-14 01:21:06 -0400

    Se agregaron templates de la primera iteracion login

--- scrum_kanban_pm/urls.py
+++ scrum_kanban_pm/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.urls import include
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='dashboard/home.html'),name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
