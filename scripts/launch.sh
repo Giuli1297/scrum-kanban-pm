485bb77a87b43df9dee8eb88725c7d8df6327abb
CBelotto <62126050+CBelotto@users.noreply.github.com>
2021-08-16 12:04:00 -0400

    Update launch.sh
    cambio de nombre de directorio

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -8,5 +8,5 @@ echo "\n\n\nACTIVACION\n\n\n"
 source ./is2_env/bin/activate
 pip list
 pip install -r requirements.txt
-cd project-item-management-system
+cd scrum_kanban_pm
 sudo -u postgres psql -c '\x' -c "ALTER USER postgres WITH PASSWORD 'postgres';"
