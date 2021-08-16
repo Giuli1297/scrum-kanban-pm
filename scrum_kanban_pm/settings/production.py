ccb82f0a7950c1eb7d10f9642ee559e1e71f3fd7
Giuliano <giuli1297.gg@gmail.com>
2021-08-16 02:22:21 -0400

    Verion 1 produccion listo para deployar

--- scrum_kanban_pm/settings/production.py
+++ scrum_kanban_pm/settings/production.py
@@ -16,6 +16,27 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: update this when you have the production host
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
+INSTALLED_APPS = [
+    # 'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    #Third-party libraries
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    #'allauth.socialaccount.providers.facebook',
+    #'allauth.socialaccount.providers.github',
+
+    'crispy_forms',
+    #Local Apps
+    'projectmanager',
+]
+
 SITE_ID = env('SITE_ID')
 
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
