2d2ce0e43e5033b2afc990d913dc6df8db9092d6
Ivan Vera <ivera2065@fpuna.edu.py>
2021-09-20 10:39:03 -0400

    tests agregados

--- projectmanager/tests/test_urls.py
+++ projectmanager/tests/test_urls.py
@@ -97,4 +97,39 @@ class TestUrls(SimpleTestCase):
         Test para verificar que se carga correctamente la vista correspondiente para eliminar roles de usuarios
         """
         url = reverse('delete_rol_user', args=[1])
-        self.assertEquals(resolve(url).func.view_class, views.EliminarRolUser)
+        self.assertEquals(resolve(url).func.view_class, views.EliminarRolUser) 
+    
+     def test_createUS(self):
+            """
+        Test para verificar que se carga correctamente la vista correspondiente para crear user story
+        """
+        url = reverse('create_us', args=['slug'])
+        self.assertEquals(resolve(url).func.view_class, views.UserStoryCreate) 
+    
+    def test_createSprint(self):
+        """
+        Test para verificar que se carga correctamente la vista correspondiente para crear un sprint
+        """
+        url = reverse('crear_sprint', args=['slug'])
+        self.assertEquals(resolve(url).func.view_class, views.CrearSprint) 
+    
+    def test_listar_sprint_backlog(self):
+        """
+        Test para verificar que se carga correctamente la vista correspondiente para lista sprint backlog
+        """
+        url = reverse('sprint_backlog', args=['slug', 1])
+        self.assertEquals(resolve(url).func.view_class, views.listaUsSprintBacklog) 
+    
+    def test_planning_poker_smaster(self):
+        """
+        Test para verificar que se carga correctamente la vista correspondiente al smaster planning poker 
+        """
+        url = reverse('planning_poker_smaster', args=['slug'])
+        self.assertEquals(resolve(url).func.view_class,  views.PlanningPokerView) 
+    
+    def test_planning_poker_estimar_sprint(self):
+        """
+        Test para verificar que se carga correctamente la vista correspondiente planning poker, estimación de sprint 
+        """
+        url = reverse('estimar_sprint', args=['slug'])
+        self.assertEquals(resolve(url).func.view_class, views.EstimarSprint)
