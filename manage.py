66546c5ee46d29e2e283cb5ece583f0f00e38424
Giuliano <giuli1297.gg@gmail.com>
2021-09-19 23:35:32 -0400

    Probando heroku

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrum_kanban_pm.settings.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrum_kanban_pm.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
