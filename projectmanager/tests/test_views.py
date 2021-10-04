7e2210b813649f5564c2c436cfbbf59d4e49f78c
Giuliano <giuli1297.gg@gmail.com>
2021-10-04 16:04:23 -0300

    cread admin fixed

--- projectmanager/tests/test_views.py
+++ projectmanager/tests/test_views.py
@@ -1,13 +1,13 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, SimpleTestCase
 
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User, Permission, Group
 from django.test import TestCase
 from .. import views
 from django.urls.base import reverse, resolve
 # Create your tests here.
 from allauth.account.forms import BaseSignupForm, ResetPasswordForm, SignupForm
-from ..models import Proyecto
-from projectmanager.forms import ProyectoForm
+from ..models import Proyecto, Rol
+from projectmanager.forms import *
 
 
 class TestViews(TestCase):
@@ -21,31 +21,44 @@ class TestViews(TestCase):
         Deja listas las variables para que se utilicen en los tests.
 
         """
-        self.client = Client()
-        self.my_admin = User(username='user', is_staff=True, is_superuser=True)
-        self.my_admin.set_password('passphrase')  # can't set above because of hashing
-        self.my_admin.save()  # needed to save to temporary test db
-        response = self.client.get('/admin/', follow=True)
-        loginresponse = self.client.login(username='user', password='passphrase')
-        self.proyecto = Proyecto.objects.create(nombre='slug', descripcion='xd', scrum_master=self.my_admin)
-        self.assertTrue(loginresponse)
+        self.admin = User.objects.create_user(username='admin1', email='giuli@email.com', password='admin1')
+        self.admin.is_staff = True
+        admin_group = Group.objects.create(name='Administrador')
+        admin_rol = Rol.objects.create(related_group=admin_group, tipo='sistema')
+        for permission in Permission.objects.all():
+            admin_group.permissions.add(permission)
+        self.admin.groups.add(admin_group)
+        self.client.login(username='admin1', password='admin1')
+        self.proyecto = Proyecto.objects.create(nombre='slug', descripcion='xd', scrum_master=self.admin)
 
     def test_home_GET(self):
         """
         Testea que el template de home se cargue correctamenta
 
         """
-        response = self.client.get(reverse('home'))
+        url = reverse('home')
+        response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'dashboard/home.html')
 
-    def test_proyecto_list_GET(self):
+    def test_verification_account_GET(self):
         """
-        Prueba que devuelva el template correcto
+        Test de la vista
         """
-        response = self.client.get(reverse('proyecto_listar'))
+        url = reverse('activate', kwargs={
+            'uidb64': 'uid', 'token': 'token'})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_system_activity_GET(self):
+        """
+
+        :return:
+        """
+        url = reverse('system_activity')
+        response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'proyecto/proyecto_list.html')
+        self.assertTemplateUsed(response, 'dashboard/system_activity.html')
 
     def test_proyecto_crear_GET(self):
         """
@@ -54,6 +67,41 @@ class TestViews(TestCase):
         response = self.client.get(reverse('proyecto_crear'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'proyecto/proyecto_form.html')
+
+    def test_proyecto_crear_POST(self):
+        """
+        
+        :return: 
+        """
+        url = reverse('proyecto_crear')
+        response = self.client.post(url, data={
+            'nombre': 'proyecto test3',
+            'descripcion': 'test',
+            'scrum_master': User.objects.all()[1]}, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+    def test_proyecto_agregar_sm_POST(self):
+        """
+
+        :return:
+        """
+        url = reverse('proyecto_agregar_sm', args=['slug'])
+        response = self.client.post(url, {
+            'scrum_member': self.admin,
+            'lunes': 1,
+            'martes': 1,
+            'miercoles': 1,
+            'jueves': 1,
+            'viernes': 1}, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+    def test_proyecto_list_GET(self):
+        """
+        Prueba que devuelva el template correcto
+        """
+        response = self.client.get(reverse('proyecto_listar'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'proyecto/proyecto_list.html')
 
     def test_proyecto_editar_GET(self):
         """
