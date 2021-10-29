251704b4a466b5f0a7e5e97f1a5a3290cd2cc92e
Giuliano <giuli1297.gg@gmail.com>
2021-10-29 11:16:28 -0300

    bug fix

--- projectmanager/management/commands/modify_site_dev.py
+++ projectmanager/management/commands/modify_site_dev.py
@@ -2,6 +2,7 @@ import requests
 from django.contrib.auth.models import Permission, Group, User
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
 
 
 class Command(BaseCommand):
@@ -9,6 +10,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
+            sapp = SocialApp(provider='google', name='scrum-kanban',
+                             client_id='502746393109-njpkkl1tlfrhdki9i1jkgdaitga2knic.apps.googleusercontent.com',
+                             secret='Z1sNtrKMTOGMIR-UZ20vTUF5')
+            sapp.save()
             site = Site.objects.all()[0]
             site.domain = '127.0.0.1:8080'
             site.name = '127.0.0.1:8080'
